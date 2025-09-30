b93d6298eec99c03f570cc7efea302dc3119cec3
Шельмин Артём <Shelmin85@Mail.ru>
2025-09-30 17:34:30 +0700

    Имя студента

--- app.py
+++ app.py
@@ -383,6 +383,7 @@ def add_flowers(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Шельмин Артём'
+    return render_template('example.html', name=name)
 
 
