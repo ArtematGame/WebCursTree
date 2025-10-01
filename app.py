25257b3a37f50222eb54cadf684657d95c5f39af
Шельмин Артём <Shelmin85@mail.ru>
2025-10-01 18:33:15 +0700

    рефакторинг

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, url_for, request, redirect, abort, render_template
 import datetime
 from lab1 import lab1
 from lab2 import lab2
+from lab3 import lab3
 
 app = Flask(__name__)
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
+app.register_blueprint(lab3)
 
 count = 0
 
