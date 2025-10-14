ccd621a5a3d0970143f6c5072204c7fe40099969
Шельмин Артём <shelmin85@mail.ru>
2025-10-15 00:43:07 +0700

    очистка куки

--- lab3.py
+++ lab3.py
@@ -92,6 +92,15 @@ def settings():
     line_height = request.cookies.get('line_height')
     return render_template('lab3/settings.html', color=color, bg_color=bg_color, font_size=font_size, line_height=line_height)
 
+@lab3.route('/lab3/settings_clear')
+def settings_clear():
+    resp = make_response(redirect('/lab3/settings'))
+    resp.delete_cookie('color')
+    resp.delete_cookie('bg_color')
+    resp.delete_cookie('font_size')
+    resp.delete_cookie('line_height')
+    return resp
+
 @lab3.route('/lab3/ticket')
 def ticket():
     return render_template('lab3/ticket.html')
