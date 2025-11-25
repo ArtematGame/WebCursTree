040331cc42f04e71962b380b3508acddd0606d1d
Шельмин Артём <Shelmin85@mail.ru>
2025-11-25 08:57:38 +0700

    лабораторная работа 6

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
+from lab6 import lab6
 
 app = Flask(__name__)
 
@@ -50,6 +51,9 @@ def index():
                 </ul>
                 <ul>
                     <li><a href="/lab5">Пятая лабораторная</a></li>
+                </ul>
+                <ul>
+                    <li><a href="/lab6">Шестая лабораторная</a></li>
                 </ul>
             </main>
     
