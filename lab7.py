7d5b82718b03fb8c95f9e73b43e3760a9bb4ea76
Шельмин Артём <shelmin85@mail.ru>
2025-12-09 17:48:07 +0700

    исправление подключения

--- lab7.py
+++ lab7.py
@@ -5,16 +5,32 @@ from os import path
 
 lab7 = Blueprint('lab7', __name__)
 
+def db_connect():
+    if current_app.config['DB_TYPE'] == 'postgres':
+        conn = psycopg2.connect(
+            host='127.0.0.1',
+            database='artem_shelmin_knowledge_base',
+            user='artem_shelmin_knowledge_base',
+            password='123'
+        )
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+    else:
+        dir_path = path.dirname(path.realpath(__file__))
+        db_path = path.join(dir_path, "database.db")
+        conn = sqlite3.connect(db_path)
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+
+    return conn, cur
+
+def db_close(conn, cur):
+    conn.commit()
+    cur.close()
+    conn.close()
+
 @lab7.route('/lab7/')
 def main():
     return render_template('lab7/index.html') 
-
-def db_connect():
-    dir_path = path.dirname(path.realpath(__file__))
-    db_path = path.join(dir_path, "database.db")
-    conn = sqlite3.connect(db_path)
-    conn.row_factory = sqlite3.Row
-    return conn
 
 @lab7.route('/lab7/rest-api/films/', methods=['GET'])
 def get_films():
